14a1ff8d6c6db3a08ebb1c35e9451c440215beff
oliv <olivier.ferreira@alcatel-lucent.com>
2012-12-26 23:12:54 +0100

    Detection de /fn Commencement des tests avec un tableau de caractere apres le \

--- main.c
+++ main.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define MAX 100
 /* 
 
 					/!\ A FAIRE /!\ 
 
 Fonction qui prend un nom de fichier en parametre et qui l'ouvre
-Gérer le double étoiles pour le commencement d'un commentaire (/**) possible avec les fonctions ftell?fseek? gérer le seek_cur-2.
 Faut-il faire le /// sachant que ça compile pas? 
 
 */
 
 
+
+												/* Remove for testing the board */
+/*void init_tab(int tab[MAX]){
+	int i =0;
+	for(i=0;i<MAX;i++)
+		tab[i] = 0;
+}
+void affichage(int tab[MAX]){
+	int i=0;
+	printf("**********************\n");
+	printf("INITIALISATION TABLEAU\n");
+	printf("**********************\n");
+	for(i=0;i<MAX;i++){
+		
+		printf("tab[%d] = %d \n",i,tab[i]);
+	}
+	printf("**********************\n");
+	printf("	FIN TABLEAU\n");
+	printf("**********************\n");
+}*/
+												/* Remove for testing the board2 */
 /*void open_file(FILE *file){
 	*file = fopen(*file,"r");
 	fclose(*file);
@@ -22,48 +42,78 @@ int main(int argc,char* argv[]){
 	printf("Test\n");
 	open_file(test);
 	*/
-	int car=0,tmp=0,com=0,car1=0;
+	int car=0,car_before1=0,com=0,car_after1=0;
 	int espace = 0,retouralaligne = 0;
-	long curseur = 0;
+												/* Remove for testing the board2 */
+	/*int tab[MAX];
+	init_tab(&tab[0]);
+	affichage(&tab[0]);
+	*/
+												/* Remove for testing the board2 */
+	int i=0;
 	FILE* doc = fopen("doc.txt","r");
 	if (doc==NULL)
 		exit(1);
 	else{
-		fprintf(doc,"Hello World");
+		
 		do{
-			if (car == 0)
-				tmp = car;
+			
 			car = fgetc(doc);
 
-			if(tmp == '/' && car == '*' ){
-				car1 = fgetc(doc);
-				if(car1 == car && com == 0){
+			/* 
+				Car = Caractère actuel , car_before1 = car-1 et car_after1 = car+1 j'ai fait sa pour détecter les /**,les /fn
+				Ex : 
+					- /fn => car_before1 = '/' , car = 'f' , car_after1 = 'n' 
+			*/
+
+			if(car_before1 == '/' && car == '*'){
+				car_after1 = fgetc(doc);
+				if(car_after1 == car && com == 0){
 					printf("COMMENCEMENT COMMENTAIRE\n");
 					com = 1;
-					printf(" bool com = %d\n",com);
+					printf("bool com = %d\n",com);
 				}
 			}
-				
-				printf("\t\t tmp = %c , car = %c , car1 = %c \n",tmp,car,car1);
+			/* TEST => /!\ IF CAR = \ WE WANT TO KEEP EVERY CHARACTERE IN A BOARD UP TO THE CHARACTERE \n */
+			/*if(com == 1){
+				if(car == '\\'){
+					do{
+						tab[i] = fgetc(doc);
+						i++;
+						if(tab[i] != ' ' && tab[i] != '*' && tab[i] != 0)
+							printf("tab[%d] = %d \n",i,tab[i]);
+					}while(tab[i] != 10);
+					
+				}
+			}*/
+			if(com == 1 ){
+				if(car_before1 == '\\' && car == 'f'){
+					car_after1 = fgetc(doc);
+					
+					if(car_after1 == 'n'){
+						printf("\tFonction prototype \n");
+					}
+				}
+				if(car == ' ')
+					espace++;
+				if(car == '\n')
+					retouralaligne++;
+			}
 			
-			if(car == ' ')
-				espace++;
-			if(car == '\n')
-				retouralaligne++;
-			
-			if(tmp == '*' && car == '/' && com == 1){
+			if(car_before1 == '*' && car == '/' && com == 1){
 				printf("FERMETURE COMMENTAIRE\n");	
 				com = 0;
 				printf(" bool com2 = %d\n",com);
+				return 0;
 			}
 			
-			tmp = car;
+			car_before1 = car;
 			
 		}while(car != EOF);
 		
 		fclose(doc);
 	}
 
-printf("espace = %d , retour a la ligne = %d \n",espace,retouralaligne);
+
 return 0;
 }
